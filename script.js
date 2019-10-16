bee28109730dbacd212aef79469c3c2a262c86ff
Jakob Blaschke <jakobblaschke@yahoo.de>
2019-10-16 18:21:52 +0200

    Add comments

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ function createGrid(size) {
   for (let i = 0; i < (size**2); i++) {
     let div = document.createElement('div');
     // div.innerHTML = i;
-    div.classList.add('pixel');
-    div.addEventListener('mouseenter', vanish);
-    container.appendChild(div);
+    div.classList.add('pixel');                               // Give every little div the class .pixel
+    div.addEventListener('mouseenter', vanish);               // attach the default EventListener
+    container.appendChild(div);                               // Append Elements to container
   }
 
   let gridRule = `
   #container {
     display: grid;
     grid-template-columns: repeat(${size}, 1fr);
-  }`;
+  }`;   // Defines a CSS rule for the container which gets applied via JS.
 
-  let pixelSize = (560/size)-(2*pixelBorderWidth);
+  let pixelSize = (560/size)-(2*pixelBorderWidth);      // 560 is the total width of container in this case
 
   let pixelRule = `
   .pixel {
@@ -28,9 +28,9 @@ function createGrid(size) {
     height: ${pixelSize}px;
     width: ${pixelSize}px;
     background-color: #26173f;
-  }`;
+  }`;   // Analogous to before this is the CSS rule for the individual pixels, to be able to modifiy sizes from the script.
 
-  let style = document.createElement('style');
+  let style = document.createElement('style');      // Add CSS rules
   document.head.appendChild(style);
   style.sheet.insertRule(gridRule);
   style.sheet.insertRule(pixelRule);
@@ -41,7 +41,7 @@ function vanish(e) {
   if (e.ctrlKey) {
     return
   } else {
-    e.target.style.transition = 'all 0.7s ease-out';
+    e.target.style.transition = 'all 0.7s ease-out';        // Constant Fade-out effect
     e.target.style.backgroundColor = 'transparent';
     e.target.style.borderColor = 'transparent';
   }
@@ -51,8 +51,7 @@ function resetGrid() {
   const pixels = document.querySelectorAll('.pixel');
 
   pixels.forEach((node) => {
-    node.style.transition = `${Math.random() * 2}s`;
-    node.classList.remove('hidden');
+    node.style.transition = `${Math.random() * 2}s`;        // To make the pixels reappear randomly
     node.style.backgroundColor = '#26173f';
     node.style.borderColor = `seagreen`;
     node.style.opacity = 1;
@@ -64,40 +63,39 @@ function resizeGrid() {
   gridSize = prompt('Please enter a size for the new Grid. (Between 1 and 64)');
   if (gridSize > 64) {
     resizeGrid();
-  } else if (gridSize) {
-    while(container.firstChild) {
+  } else if (gridSize && typeof gridSize === 'number') {
+    while(container.firstChild) {                           // Remove old divs and redraw the grid only if a number lower than 65 was entered
       container.firstChild.remove();
     }
-
     createGrid(gridSize);
-  } else {
+  } else {                                                  // So you can cancel the Resizing
     return
   }
 
-  if (randomColorsActivated) {
+  if (randomColorsActivated) {                              // To reactivate randomColor Mode if it was activated
     RandomColor();
     RandomColor();
-  } else if (gradientActivated) {
+  } else if (gradientActivated) {                           // To reactivate gradient Mode if it was activated
     Gradient();
     Gradient();
   }
 }
 
 function RandomColor() {
-  if (gradientActivated) {      // only one mode can be activated at a time
+  if (gradientActivated) {                                  // Only one mode can be activated at a time
     Gradient();
   }
 
   const pixels = document.querySelectorAll('.pixel');
-  randomColorsActivated = !randomColorsActivated;
-  randomColorButton.classList.toggle('activeButton');
+  randomColorsActivated = !randomColorsActivated;           // Toggle randomColor Mode 
+  randomColorButton.classList.toggle('activeButton');       // Toggle randomColorButton 
 
-  if (randomColorsActivated) {
+  if (randomColorsActivated) {                              // If it got activated attach the randomColor event listener and remove old one
     pixels.forEach((pixel) => {
       pixel.removeEventListener('mouseenter', vanish);
       pixel.addEventListener('mouseenter', randomColor);
     });
-  } else {
+  } else {                                                  // If it got deactivated do the reverse
     pixels.forEach((pixel) => {
       pixel.removeEventListener('mouseenter', randomColor);
       pixel.addEventListener('mouseenter', vanish);
@@ -105,11 +103,11 @@ function RandomColor() {
   }
 }
 
-function randomColor(e) {
-  if (e.ctrlKey) {
+function randomColor(e) {   // This is the actual function that gets applied on RandomColor Mode
+  if (e.ctrlKey) {          // Deactivate 'Drawing' when holding Ctrl
     return
   } else {
-    e.target.style.transition = 'all 0.7s ease-out';
+    e.target.style.transition = 'all 0.7s ease-out';    // Apply constant transition after it was randomised by a reset
     e.target.style.opacity = 1;
     e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
     e.target.style.borderColor = 'transparent';
@@ -122,8 +120,8 @@ function Gradient() {
   }
 
   const pixels = document.querySelectorAll('.pixel');
-  gradientActivated = !gradientActivated;
-  gradientButton.classList.toggle('activeButton');
+  gradientActivated = !gradientActivated;                   // Toggle Gradient Mode 
+  gradientButton.classList.toggle('activeButton');          // Toggle GradientButton
 
   if (gradientActivated) {
     pixels.forEach((pixel) => {
@@ -138,29 +136,32 @@ function Gradient() {
   }
 }
 
-function gradient(e) {
-  if (e.ctrlKey) {
+function gradient(e) {  // This is the actual function that gets applied in GradientMode
+  if (e.ctrlKey) {      // Hold Ctrl to stop 'drawing'
     return
-  } else if (e.target.style.backgroundColor !== 'black') {
+  } else if (e.target.style.backgroundColor !== 'black') {    // The first time you draw, make background 'Black' but opacity really low, so it looks white
     e.target.style.transition = 'none';
     e.target.style.opacity = 0.1;
     e.target.style.backgroundColor = 'black'; 
     e.target.style.borderColor = 'transparent';
   } else {
-    e.target.style.transition = 'all 0.7s ease-out';
-    e.target.style.opacity = Number(e.target.style.opacity) + 0.1;
+    e.target.style.transition = 'all 0.7s ease-out';          // To have a constant transition after it got randomised by a Reset
+    e.target.style.opacity = Number(e.target.style.opacity) + 0.1;    // Darken the Pixel by 10%
   }
 }
 
-
+// Initial values
 let gridSize = 16;
 let pixelBorderWidth = 1;
 let randomColorsActivated = false;
 let gradientActivated = false;
 
+// Apply Event Handlers for the buttons
 resetButton.addEventListener('click', resetGrid);
 resizeButton.addEventListener('click', resizeGrid);
 randomColorButton.addEventListener('click', RandomColor);
 gradientButton.addEventListener('click', Gradient);
+
+// Initialize Grid when loading page
 createGrid(gridSize);
 
