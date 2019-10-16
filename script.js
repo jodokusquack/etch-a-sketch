e3565936c4f646f934beb5126673e91a23d5ba65
Jakob Blaschke <jakobblaschke@yahoo.de>
2019-10-16 13:16:24 +0200

    Add RandomColor button functionality

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ function createGrid(size) {
     grid-template-columns: repeat(${size}, 1fr);
   }`;
 
-  let pixelBorderWidth = 1;
   let pixelSize = (560/size)-(2*pixelBorderWidth);
 
   let pixelRule = `
@@ -43,7 +42,8 @@ function vanish(e) {
     return
   } else {
     e.target.style.transition = 'all 0.7s ease-out';
-    e.target.classList.add('hidden');
+    e.target.style.backgroundColor = 'transparent';
+    e.target.style.borderColor = 'transparent';
   }
 }
 
@@ -53,31 +53,52 @@ function resetGrid() {
   pixels.forEach((node) => {
     node.style.transition = `${Math.random() * 2}s`;
     node.classList.remove('hidden');
+    node.style.backgroundColor = '#26173f';
+    node.style.borderColor = `seagreen`;
     }
   );
 }
 
 function resizeGrid() {
-  let newSize = prompt('Please enter a size for the new Grid. (Between 1 and 64)');
-  if (newSize > 64) {
+  gridSize = prompt('Please enter a size for the new Grid. (Between 1 and 64)');
+  if (gridSize > 64) {
     resizeGrid();
-  } else if (newSize) {
+  } else if (gridSize) {
     while(container.firstChild) {
       container.firstChild.remove();
     }
 
-    createGrid(newSize);
+    createGrid(gridSize);
   } else {
     return
   }
 }
 
 function randomColors() {
-  
+  const pixels = document.querySelectorAll('.pixel');
+
+  pixels.forEach((pixel) => {
+    pixel.removeEventListener('mouseover', vanish);
+    pixel.addEventListener('mouseover', randomColor);
+  })
+
+  function randomColor(e) {
+    if (e.ctrlKey) {
+      return
+    } else {
+      e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
+      e.target.style.borderColor = 'transparent';
+    }
+    
+  }
 }
 
 
+let gridSize = 16;
+let pixelBorderWidth = 1;
+
 resetButton.addEventListener('click', resetGrid);
 resizeButton.addEventListener('click', resizeGrid);
-createGrid(16);
+randomColorButton.addEventListener('click', randomColors);
+createGrid(gridSize);
 
